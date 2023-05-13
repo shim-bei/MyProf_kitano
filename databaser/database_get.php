af74e9d04fdd5903a3c0f41366fc0be887447805
Shim-bei <124365373+shim-bei@users.noreply.github.com>
2023-05-13 23:17:05 +0900

    Update database_get.php

--- databaser/database_get.php
+++ databaser/database_get.php
@@ -6,6 +6,9 @@ $conn = new mysqli('host', 'username', 'database-name', 'password');
 // データを取得
 $sql = "SELECT id, maguro, uni, ebi, tamago, ikura, ika FROM sushineta";
 $result = $conn->query($sql);
+// 現在時刻をユニックスタイムで取得し、データ量削減のために上２桁を削る
+$time=time();
+$time = substr($time, 3,10);
 
 // データを出力
 if ($result->num_rows > 0) {
@@ -13,6 +16,15 @@ if ($result->num_rows > 0) {
     $output = array();
     while($row = $result->fetch_assoc()) {
         $output[] = $row;
+        
+        //現在の時刻とデータベースに書き込まれた時刻の差をとる         
+        $diff = $time - $row['nowtime'];
+        //データベースに格納されてから一定時間がたっていればデータを消去
+        if($diff > 72000){
+            $sqld = "DELETE FROM sushineta";
+            // SQL文を実行
+            mysqli_query($conn, $sqld);    
+        }
     }
 
     // JSON形式で出力
